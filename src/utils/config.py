24d52a5a52d11dd3a0c8fdbcf7794810e6722dd9
Minter <adf5858@naver.com>
2025-06-20 16:09:30 +0900

    Style: 코드 스타일 수정

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,18 +3,18 @@ import os
 from pathlib import Path
 from shutil import copyfile
 
+
 class Config:
-    def __init__(self,config_path = "../../config/config.ini"):
+    def __init__(self, config_path="../../config/config.ini"):
         self.config = configparser.ConfigParser()
         self.config_path = config_path
         self.load_config()
-        
-    
+
     # config.ini 로드
     def load_config(self):
         if not os.path.exists(self.config_path):
             self.create_config()
-    
+
     # config.ini 생성
     def create_config(self):
         template_path = Path("../../config/config.ini.template")
